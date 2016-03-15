060f0665e23b9ed7a05d4345d4df93f83d526aea
glejeune <gregoire.lejeune@free.fr>
2016-03-15 12:31:11 +0100

    Remove (temporary) elixir bindings

--- lib/Buccode.ex
+++
@@ -1,9 +0,0 @@
-# File: lib/Buccode.ex
-# This file was generated from src/buccode.erl
-# Using mix.mk (https://github.com/botsunit/mix.mk)
-# MODIFY IT AT YOUR OWN RISK AND ONLY IF YOU KNOW WHAT YOU ARE DOING!
-defmodule Buccode do
-	def unquote(:"priv_dir")(arg1) do
-		:erlang.apply(:"buccode", :"priv_dir", [arg1])
-	end
-end
