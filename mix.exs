56d6ee7cecbeba0bbc7075c048bfb782eed2ee1a
glejeune <gregoire.lejeune@free.fr>
2017-02-05 20:46:36 +0100

    Bump to 1.0.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bucs.Mixfile do
   def project do
     [
       app: :bucs,
-      version: "1.0.6",
+      version: "1.0.7",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
