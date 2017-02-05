f6260e535b4e4c2197f2543874597ba06417adbe
glejeune <gregoire.lejeune@free.fr>
2017-02-05 19:54:12 +0100

    Update rebar3_elixir for Elixir 1.4

--- mix.exs
+++ mix.exs
@@ -8,8 +8,8 @@ defmodule Bucs.Mixfile do
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps,
-      aliases: aliases
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
